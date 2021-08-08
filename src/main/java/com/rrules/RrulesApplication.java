9ce53dd7144193f943c2463745f5df32bf8b5cb6
bboyKamel <nacoon@tlen.pl>
2021-08-08 09:02:20 +0200

    start

---
+++ src/main/java/com/rrules/RrulesApplication.java
@@ -0,0 +1,13 @@
+package com.rrules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rrules/RrulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
