64f9c3ad57c59bd7eeeadaa235a248ccc721b4e5
Przemek Przybylski <prprzyby@LCE45631.corp.capgemini.com>
2021-08-09 14:34:22 +0200

    "add two new endpoints "

--- src/main/java/com/rrules/controllers/NewsServiceController.java
+++ src/main/java/com/rrules/controllers/NewsServiceController.java
@@ -2,6 +2,7 @@
 package com.rrules.controllers;
 
 import com.rrules.services.RandomMessageService;
+import com.rrules.web.MessageDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class NewsServiceController {
@@ -22,9 +24,26 @@ public class NewsServiceController {
     @Autowired
     RandomMessageService randomMessageService;
     
+    @Autowired
+    RestTemplate restTemplate;
+    
     @GetMapping(path = "/news", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity randomNews() {        
         String message = randomMessageService.randomMessage();        
     return new ResponseEntity(message, HttpStatus.OK);
+    }
+    
+    @GetMapping(path = "/news/good", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity goodNews() {        
+        ResponseEntity<MessageDTO> response = restTemplate
+            .getForEntity(goodNewsUrl, MessageDTO.class);        
+    return new ResponseEntity(response.getBody().getMessage(), HttpStatus.OK);
+    }
+    
+    @GetMapping(path = "/news/bad", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity badNews() {        
+        ResponseEntity<MessageDTO> response = restTemplate
+            .getForEntity(badNewsUrl, MessageDTO.class);        
+    return new ResponseEntity(response.getBody().getMessage(), HttpStatus.OK);
     }
 }
