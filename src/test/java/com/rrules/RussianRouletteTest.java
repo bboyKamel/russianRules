dfba7caed12e638bd674202acc1efc62561aca6e
Przemek Przybylski <prprzyby@LCE45631.corp.capgemini.com>
2021-08-16 12:18:29 +0200

    "feat: add russian roulette unit tests"

--- src/test/java/com/rrules/RussianRouletteTest.java
+++ src/test/java/com/rrules/RussianRouletteTest.java
@@ -3,24 +3,34 @@ package com.rrules;
 
 import com.rrules.services.RussianRouletteService;
 import com.rrules.services.RussianRouletteServiceImpl;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class RussianRouletteTest {
     
+    RussianRouletteService rr = new RussianRouletteServiceImpl();
+    
+    @Test
+    public void contextLoads() throws Exception {
+	assertThat(rr).isNotNull();
+    }  
+    
+    @Test
+    private void shouldReturnCorrectTypeTest() {        
+
+    }
+    
     @Test
     public void shouldReturnOneInFiveProbabilityTest() {
-        
-        RussianRouletteService rr = new RussianRouletteServiceImpl();
-        
+
         int countBad = 0;
         
         for(int i=0; i < 1000; i++){
             if (rr.randomSpin() == false) countBad++;
         }
-        System.out.println(countBad);
+        
         assertTrue(countBad >= 150 && countBad <= 200);
     }
-    
 
 }
